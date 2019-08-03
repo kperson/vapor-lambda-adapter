88f89316d4a0769dd2da7ee94e46032c74d505ae
Kelton Person <kelton.person@gmail.com>
2019-08-03 15:42:57 -0500

    added level support allow for more configurable logging

--- Sources/LambdaLogger.swift
+++ Sources/LambdaLogger.swift
@@ -14,17 +14,45 @@ import Glibc
 import Darwin.C
 #endif
 
+extension LogLevel {
+    
+    var rank: Int {
+        switch self {
+        case .verbose: return 1
+        case .debug: return 2
+        case .info: return 3
+        case .warning: return 4
+        case .error: return 5
+        case .fatal: return 6
+        default: return 7
+        }
+    }
+    
+}
 
 public class LambdaLogger: Logger, Service {
     
-    public init() {
+    private let levelThreshold: LogLevel
+    
+    public init(level: LogLevel? = nil) {
+        if let l = level {
+            self.levelThreshold = l
+        }
+        else if let level = ProcessInfo.processInfo.environment["LOG_LEVEL"] {
+            self.levelThreshold = LogLevel(stringLiteral: level)
+        }
+        else {
+            self.levelThreshold = .info
+        }
     }
     
     let printLogger = PrintLogger()
     
     public func log(_ string: String, at level: LogLevel, file: String, function: String, line: UInt, column: UInt) {
-        printLogger.log(string, at: level, file: file, function: function, line: line, column: column)
-        fflush(stdout)
+        if level.rank >= levelThreshold.rank {
+            printLogger.log(string, at: level, file: file, function: function, line: line, column: column)
+            fflush(stdout)
+        }
     }
     
 }
